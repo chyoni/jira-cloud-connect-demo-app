2a1207a07ca2730433a79e8986e7d25a8cd6c2fd
cwchoi <cwchoi@osci.kr>
2025-03-12 09:55:32 +0900

    @EnableJpaRepositories

--- src/main/java/kr/osci/addons/app/AddonApplication.java
+++ src/main/java/kr/osci/addons/app/AddonApplication.java
@@ -2,8 +2,10 @@ package kr.osci.addons.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "kr.osci.addons.app.domain.board")
 public class AddonApplication {
 
     public static void main(String[] args) {
