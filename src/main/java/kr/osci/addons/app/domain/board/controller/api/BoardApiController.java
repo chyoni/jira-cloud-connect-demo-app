6c9ad2ad86df2f65c237b3f92eab66b717404452
cwchoi <cwchoi@osci.kr>
2025-03-14 17:05:29 +0900

    123
    q

--- src/main/java/kr/osci/addons/app/domain/board/controller/api/BoardApiController.java
+++ src/main/java/kr/osci/addons/app/domain/board/controller/api/BoardApiController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
-@IgnoreJwt
+//@IgnoreJwt
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/board")
