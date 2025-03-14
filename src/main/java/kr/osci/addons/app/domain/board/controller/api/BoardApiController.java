36818aab9cc4f738f6679e0157aafce7e3973f1b
cwchoi <cwchoi@osci.kr>
2025-03-14 16:52:40 +0900

    fe

--- src/main/java/kr/osci/addons/app/domain/board/controller/api/BoardApiController.java
+++ src/main/java/kr/osci/addons/app/domain/board/controller/api/BoardApiController.java
@@ -1,6 +1,7 @@
 package kr.osci.addons.app.domain.board.controller.api;
 
 import com.atlassian.connect.spring.AtlassianHostUser;
+import com.atlassian.connect.spring.IgnoreJwt;
 import kr.osci.addons.app.domain.board.service.ArticleService;
 import kr.osci.addons.app.domain.board.service.request.ArticleCreateRequest;
 import lombok.RequiredArgsConstructor;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
+@IgnoreJwt
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/board")
