9dcf8affd007f42b3939556d0cac1f6bcff80598
cwchoi <cwchoi@osci.kr>
2025-03-15 13:20:02 +0900

    v

--- src/main/java/kr/osci/addons/app/domain/board/service/ArticleService.java
+++ src/main/java/kr/osci/addons/app/domain/board/service/ArticleService.java
@@ -1,5 +1,7 @@
 package kr.osci.addons.app.domain.board.service;
 
+import com.atlassian.connect.spring.AtlassianHost;
+import com.atlassian.connect.spring.AtlassianHostRestClients;
 import com.atlassian.connect.spring.AtlassianHostUser;
 import kr.osci.addons.app.domain.board.entity.Article;
 import kr.osci.addons.app.domain.board.repository.ArticleRepository;
@@ -10,6 +12,8 @@ import kr.osci.addons.app.domain.board.service.response.ArticleReadResponse;
 import kr.osci.addons.app.domain.board.utils.PageLimitCalculator;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,10 +25,17 @@ import java.util.List;
 @Transactional(readOnly = true)
 public class ArticleService {
     private final ArticleRepository articleRepository;
+    private final AtlassianHostRestClients atlassianHostRestClients;
 
     @Transactional
     public ArticleCreateResponse create(ArticleCreateRequest request, String writerId) {
         log.info("[create:27] writerId = {}", writerId);
+        ResponseEntity<String> response = atlassianHostRestClients.authenticatedAsAddon()
+                .getForEntity("https://cwchoiit.atlassian.net/rest/api/3/user/{writerId}", String.class, writerId);
+
+        log.info("[create:36] response status = {}", response.getStatusCode());
+        log.info("[create:37] response body = {}", response.getBody());
+
         Article newArticle = articleRepository.save(
                 Article.of(
                         request.title(),
