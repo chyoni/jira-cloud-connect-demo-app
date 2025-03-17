018c478f58b3e44e6b4d7443cb3ba15eb4768f67
cwchoi <cwchoi@osci.kr>
2025-03-17 23:11:30 +0900

    qwe

--- src/main/java/kr/osci/addons/app/domain/board/controller/view/BoardController.java
+++ src/main/java/kr/osci/addons/app/domain/board/controller/view/BoardController.java
@@ -39,8 +39,9 @@ public class BoardController {
         return "board/createForm";
     }
 
-    @GetMapping("/article/{articleId}")
-    public String readArticle(@PathVariable("articleId") Long articleId, Model model) {
+    @GetMapping("/article")
+    public String readArticle(@RequestParam("articleId") Long articleId, Model model) {
+        log.info("[readArticle:44] articleId={}", articleId);
         model.addAttribute("article", articleService.read(articleId));
         return "board/readArticle";
     }
